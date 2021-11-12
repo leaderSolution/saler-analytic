1b1976bb68c95a0861e8b770ca8c3417914ff016
Imen <imen.achouri.mz@gmail.com>
2021-11-12 14:12:55 +0100

    init postgres sql for testing

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,8 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    //.enableSingleRuntimeChunk()
-    .disableSingleRuntimeChunk()
+    .enableSingleRuntimeChunk()
+    //.disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
